d8cb2c44587c172993727a09549f8aa625b8cc51
Oleksii Danylevskyi <aleksey@danilevsky.com>
2017-09-02 17:46:45 +0100

    Update BST insert func

--- data_structures/binary_search_tree.rb
+++ data_structures/binary_search_tree.rb
@@ -27,6 +27,10 @@ class BinarySearchTree
     insert_node(data, root)
   end
 
+  def find(data)
+    find_node(data, root)
+  end
+
   def find_node(data, node)
     if node.nil?
       return nil
@@ -40,34 +44,6 @@ class BinarySearchTree
     end
   end
 
-  def find(data)
-    find_node(data, root)
-  end
-
-  def find_max_node(node)
-    if node.right.nil? && node.left.nil?
-      return node
-    end
-    if !node.right.nil?
-      return find_max_node(node.right)
-    end
-    if !node.left.nil?
-      return find_max_node(node.left)
-    end
-  end
-
-  def find_min_node(node)
-    if node.right.nil? && node.left.nil?
-      return node
-    end
-    if !node.left.nil?
-      return find_min_node(node.left)
-    end
-    if !node.right.nil?
-      return find_min_node(node.right)
-    end
-  end
-
   def remove_leaf(node)
     if node.parent.left == node
       node.parent.left = nil
@@ -76,52 +52,60 @@ class BinarySearchTree
     end
   end
 
-  def replace_leaf(a, b)
-    unless a.parent.nil?
-      if a.parent.left == a
-        a.parent.left = b
-      else
-        a.parent.right = b
-      end
-      b.parent = a.parent
-      b.left = a.left
-      if !b.left.nil?
-        b.left.parent = b
-      end
-      b.right = a.right
-      if !b.right.nil?
-        b.right.parent = b
-      end
-    else
-      b.parent = nil
-      b.left = a.left
-      b.right = a.right
-      self.root = b
+  def successor(node)
+    if node.left.nil?
+      remove_node(node)
+      return node
+    end
+    if !node.left.nil?
+      successor(node.left)
     end
   end
 
-  def remove_node(data, node)
-    node = find_node(data, root)
+  def remove_node(node)
     unless node.nil?
       if node.parent.nil? && node.left.nil? && node.right.nil?
         self.root = nil
       elsif node.left.nil? && node.right.nil?
         remove_leaf(node)
-      elsif !node.left.nil?
-        left_max_node = find_max_node(node.left)
-        remove_leaf(left_max_node)
-        replace_leaf(node, left_max_node)
+      elsif !node.left.nil? && !node.right.nil?
+        successor = successor(node.right)
+        successor.parent = nil
+        successor.left = node.left
+        unless successor.left.nil?
+          successor.left.parent = successor
+        end
+        successor.right = node.right
+        unless successor.right.nil?
+          successor.right.parent = successor
+        end
+        if node.parent.left == node
+          node.parent.left = successor
+        else
+          node.parent.right = successor
+        end
+      elsif node.left.nil?
+        if node.parent.left == node
+          node.parent.left = node.right
+        else
+          node.parent.right = node.right
+        end
+        node.right.parent = node.parent
       else
-        right_min_node = find_min_node(node.right)
-        remove_leaf(right_min_node)
-        replace_leaf(node, right_min_node)
+        if node.parent.left == node
+          node.parent.left = node.left
+        else
+          node.parent.right = node.left
+        end
+        node.left.parent = node.parent
       end
     end
     return node
   end
 
   def remove(data)
-    remove_node(data, root)
+    node = find_node(data, root)
+    remove_node(node)
   end
 
   def level_order_traversal
@@ -130,8 +114,8 @@ class BinarySearchTree
     while !levels.empty?
       level = levels.pop
       new_level = []
-      puts level.collect(&:data).to_s
       level.each do |node|
+        print "(DATA: #{node.data}, LEFT: #{node.left.nil? ? 'nil' : node.left.data}, RIGHT: #{node.right.nil? ? 'nil' : node.right.data}, PARENT:  #{node.parent.nil? ? 'nil' : node.parent.data}) "
         unless node.left.nil?
           new_level << node.left
         end
@@ -139,6 +123,7 @@ class BinarySearchTree
           new_level << node.right
         end
       end
+      puts ""
       unless new_level.empty?
         levels << new_level
       end
@@ -146,14 +131,20 @@ class BinarySearchTree
   end
 end
 
-bst = BinarySearchTree.new(5)
-bst.insert(15)
-bst.insert(11)
-bst.insert(12)
-bst.insert(13)
+bst = BinarySearchTree.new(44)
 bst.insert(17)
+bst.insert(32)
+bst.insert(28)
+bst.insert(29)
+bst.insert(88)
+bst.insert(97)
+bst.insert(65)
+bst.insert(54)
+bst.insert(82)
+bst.insert(76)
+bst.insert(80)
+bst.insert(78)
 
-# puts bst.remove(15).to_s
-# puts bst.remove(13).to_s
-# bst.remove(5)
+bst.remove(32)
+bst.remove(65)
 bst.level_order_traversal
